a592840083da4aadd15e5a6ca0049f8d5a12abbd
Hruden Ivan <Hruden.Ivan@gmail.com>
2021-09-30 22:27:46 +0300

    add db schema

--- sql/library_spring.sql
+++ sql/library_spring.sql
@@ -22,7 +22,6 @@ DROP TABLE IF EXISTS `library_spring`.`order_types`;
 DROP TABLE IF EXISTS `library_spring`.`order_statuses`;
 DROP TABLE IF EXISTS `library_spring`.`users_books`;
 
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -30,12 +29,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema library_spring
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema library_spring
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `library_spring` DEFAULT CHARACTER SET utf8 ;
 USE `library_spring` ;
+
 
 -- -----------------------------------------------------
 -- Table `library_spring`.`user_types`
